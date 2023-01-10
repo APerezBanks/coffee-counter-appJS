87b684f57ca298b81acddaf2a78d37df4527f2a6
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2023-01-10 01:08:52 +0100

    uploaded passenger app

---
+++ index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let cancelEl = document.getElementById("cancel-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function cancel() {
+    count -= 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " " + "☕" + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
